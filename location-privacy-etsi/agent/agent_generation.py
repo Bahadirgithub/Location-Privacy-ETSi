0b72c12be8663f878f29655327505ff361fa5735
ashxr95 <ashoo.yameen@gmail.com>
2025-11-12 17:32:00 +0100

    Ich habe eine zentrale, lesbare agent_profiles.yaml-Datei. Mein agent_generation.py-Skript liest diese YAML-Datei und erstellt die Agenten dynamisch basierend auf meine realistischen Verteilungen (18%, 7% usw.). Alle 5 Agenten-Klassen (Worker, PartTimeWorker, NightWorker, Homestay, Freelance) sind jetzt aktualisiert und akzeptieren diese Konfiguration.

--- location-privacy-etsi/agent/agent_generation.py
+++ location-privacy-etsi/agent/agent_generation.py
@@ -1,22 +1,22 @@
 # coding: utf-8
+import os, sys
+sys.path.insert(0, os.path.dirname(__file__))
+
 from model.Worker import Worker
 from model.PartTimeWorker import PartTimeWorker
 from model.NightWorker import NightWorker
 from model.Homestay import Homestay
 from model.Freelance import Freelance
+
 from utils import map_parser
 from model.Agent import *
 from model.District import *
 from model.Location import *
-from lxml import etree
-import csv
-import argparse
-import sys
-import time
-import numpy as np
-import yaml  # Importiert yaml, um Konfigurationsdateien zu lesen
-from datetime import datetime
 
+from lxml import etree
+import csv, argparse, time, numpy as np, yaml, random
+from datetime import datetime
+import random
 
 # Globale Variable für die geladene Konfiguration
 agent_config = None
@@ -36,27 +36,28 @@ def create_test_locations(map_path):
 
 
 # create and test agents
-# Diese Funktion nimmt jetzt die geladenen Profile und Prozentsätze entgegen
+# Nimmt die geladenen Profile und Prozentsätze entgegen
 def create_test_agents(number, locations, profiles, percentages):
     test_agents = []
 
-    # Berechne die genaue Anzahl für jeden Typ basierend auf den Prozentzahlen
-    number_of_parttime = int(number * percentages['parttimeworker'])
-    number_of_nightworker = int(number * percentages['nightworker'])
-    number_of_homestay = int(number * percentages['homestay'])
-    number_of_freelance = int(number * percentages['freelance'])
-    # Der Rest sind Worker
-    number_of_worker = number - (number_of_parttime + number_of_nightworker + number_of_homestay + number_of_freelance)
+    # Prozente sind Brüche (0–1). Erzeuge Stückzahlen:
+    number_of_parttime   = int(number * percentages.get('parttimeworker', 0.0))
+    number_of_nightworker = int(number * percentages.get('nightworker', 0.0))
+    number_of_homestay   = int(number * percentages.get('homestay', 0.0))
+    number_of_freelance  = int(number * percentages.get('freelance', 0.0))
+    # Rest sind Worker
+    number_of_worker = number - (number_of_parttime + number_of_nightworker +
+                                 number_of_homestay + number_of_freelance)
 
-    # Berechne die Start-Indizes für eine saubere Zuweisung
-    start_parttime = 0
+    # Start-Indizes
+    start_parttime   = 0
     start_nightworker = start_parttime + number_of_parttime
-    start_homestay = start_nightworker + number_of_nightworker
-    start_freelance = start_homestay + number_of_homestay
-    start_worker = start_freelance + number_of_freelance
+    start_homestay   = start_nightworker + number_of_nightworker
+    start_freelance  = start_homestay + number_of_homestay
+    start_worker     = start_freelance + number_of_freelance
 
-    # Debug-Ausgabe, um die Verteilung zu prüfen (kann später entfernt werden)
-    print(f"Agenten-Verteilung wird erstellt:")
+    # Debug
+    print("Agenten-Verteilung wird erstellt:")
     print(f"  PartTimeWorker: {number_of_parttime}")
     print(f"  NightWorker:    {number_of_nightworker}")
     print(f"  Homestay:       {number_of_homestay}")
@@ -66,53 +67,63 @@ def create_test_agents(number, locations, profiles, percentages):
 
     for i in range(number):
         if i < start_nightworker:
-            # --- Erstelle PartTimeWorker ---
+            # PartTimeWorker
             test_agents.append(
-                PartTimeWorker('pt_worker' + str(i),
-                               random.choice(home_district.locations),
-                               random.choice(work_district.locations),
-                               np.random.choice(locations, random.choice([1, 2, 3, 4, 5]), replace=False),
-                               profiles['parttimeworker']  # Übergibt das Konfigurations-Profil
-                               ))
+                PartTimeWorker(
+                    'pt_worker' + str(i),
+                    random.choice(home_district.locations),
+                    random.choice(work_district.locations),
+                    np.random.choice(locations, random.choice([1, 2, 3, 4, 5]), replace=False),
+                    profiles['parttimeworker']
+                )
+            )
         elif i < start_homestay:
-            # --- Erstelle NightWorker ---
+            # NightWorker
             test_agents.append(
-                NightWorker('n_worker' + str(i),
-                            random.choice(home_district.locations),
-                            random.choice(work_district.locations),
-                            random.choice(locations),
-                            np.random.choice(locations, random.choice([1, 2, 3, 4, 5]), replace=False),
-                            profiles['nightworker']  # Übergibt das Konfigurations-Profil
-                            ))
+                NightWorker(
+                    'n_worker' + str(i),
+                    random.choice(home_district.locations),
+                    random.choice(work_district.locations),
+                    random.choice(locations),
+                    np.random.choice(locations, random.choice([1, 2, 3, 4, 5]), replace=False),
+                    profiles['nightworker']
+                )
+            )
         elif i < start_freelance:
-            # --- Erstelle Homestay ---
+            # Homestay
             test_agents.append(
-                Homestay('homestay' + str(i),
-                         random.choice(home_district.locations),
-                         random.choice(locations),  # school
-                         random.choice(locations),  # grocery
-                         random.choice(locations),  # activity
-                         np.random.choice(locations, random.choice([1, 2, 3, 4, 5]), replace=False),
-                         profiles['homestay']  # Übergibt das Konfigurations-Profil
-                         ))
+                Homestay(
+                    'homestay' + str(i),
+                    random.choice(home_district.locations),
+                    random.choice(locations),  # school
+                    random.choice(locations),  # grocery
+                    random.choice(locations),  # activity
+                    np.random.choice(locations, random.choice([1, 2, 3, 4, 5]), replace=False),
+                    profiles['homestay']
+                )
+            )
         elif i < start_worker:
-            # --- Erstelle Freelance ---
+            # Freelance
             test_agents.append(
-                Freelance('freelance' + str(i),
-                          random.choice(home_district.locations),
-                          np.random.choice(locations, random.choice([1, 2, 3, 4, 5]), replace=False),
-                          profiles['freelance']  # Übergibt das Konfigurations-Profil
-                          ))
+                Freelance(
+                    'freelance' + str(i),
+                    random.choice(home_district.locations),
+                    np.random.choice(locations, random.choice([1, 2, 3, 4, 5]), replace=False),
+                    profiles['freelance']
+                )
+            )
         else:
-            # --- Erstelle Worker (Full-Time) ---
+            # Worker (Full-Time)
             test_agents.append(
-                Worker('worker' + str(i),
-                       random.choice(home_district.locations),
-                       random.choice(work_district.locations),
-                       random.choice(locations),
-                       np.random.choice(locations, random.choice([1, 2, 3, 4, 5]), replace=False),
-                       profiles['worker']  # Übergibt das Konfigurations-Profil
-                       ))
+                Worker(
+                    'worker' + str(i),
+                    random.choice(home_district.locations),
+                    random.choice(work_district.locations),
+                    random.choice(locations),
+                    np.random.choice(locations, random.choice([1, 2, 3, 4, 5]), replace=False),
+                    profiles['worker']
+                )
+            )
     return test_agents
 
 
@@ -121,7 +132,7 @@ def generate_demand(agents, duration):
     demand = []
     for a in agents:
         demand += a.generate_demand(duration)
-    demand.sort(key=lambda x: int(x.depart), reverse=False)
+    demand.sort(key=lambda x: int(x.depart))
     return demand
 
 
@@ -133,8 +144,7 @@ def generate_demand_file(filename, agents, duration):
                                   length='5.0', maxSpeed='50.0', sigma='0.0'))
     demand = generate_demand(agents, duration)
     for d in demand:
-        # Cannot use the typical constructor etree.Element('trip',id=...) because the word from is a Python keyword which cannot be used outside of string declarations
-        # I.e. the string 'from = d.start.edge_id' is not possible
+        # 'from' ist Schlüsselwort; daher set()
         new = etree.Element('trip')
         new.set('id', d.id)
         new.set('type', d.agent.id)
@@ -150,13 +160,15 @@ def generate_demand_file(filename, agents, duration):
 def generate():
     test_locations = create_test_locations(in_path + mapin)
 
-    # Übergibt die geladenen Profile und Prozentsätze an die Erstellungs-Funktion
-    test_agents = create_test_agents(number_of_agents,
-                                     test_locations,
-                                     agent_config['agent_profiles'],
-                                     agent_config['agent_distribution'])
+    test_agents = create_test_agents(
+        number_of_agents,
+        test_locations,
+        agent_config['agent_profiles'],
+        agent_config['agent_distribution']
+    )
 
     generate_demand_file(out_path + routesout, test_agents, number_of_days)
+
     veh_map_path = out_path + vehmapout
     with open(veh_map_path, 'w', newline='') as veh_map_file:
         writer = csv.writer(veh_map_file)
@@ -170,69 +182,71 @@ def generate():
 def report():
     time1 = time.time()
     rep_end = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    f = open(rep_path + rep_name, 'a')
-    f.write('-------------------- Report of AGENT GENERATION --------------------\n\n')
-    f.write('Name of simulation script:   ' + sys.argv[0] + '\n')
-    f.write('Evaluation started at        ' + str(rep_start) +'\n')
-    f.write('Evaluation ended at          ' + str(rep_end) +'\n')
-    f.write('Runtime:                     ' + str(time1 - time0) + '\n\n')
-    f.write('Agent Config File:   ' + str(args.agent_config_path) + '\n') # Zeigt an, welche Konfig verwendet wurde
-    f.write('Map used:            ' + str(mapin) + '\n\n')
-    f.write('Number of agents:    ' + str(number_of_agents) + '\n')
-    # Hier könnten wir die Verteilung aus der Config ins Log schreiben
-    f.write('Agent Distribution:\n')
-    for key, value in agent_config['agent_distribution'].items():
-        f.write(f'  {key}: {value}%\n')
-
-    f.write('Number of days:      ' + str(number_of_days) + '\n\n')
-    f.write('Routing written to       ' + str(out_path + routesout) + '\n')
-    f.write('Vehicle map written to   ' + str(in_path + vehmapout) + '\n\n')
-    f.write('-------------------------- END of report ---------------------------\n\n')
-    print('Report written to ' + "'" + rep_path + rep_name + "'")
+    with open(rep_path + rep_name, 'a', encoding='utf-8') as f:
+        f.write('-------------------- Report of AGENT GENERATION --------------------\n\n')
+        f.write('Name of script:             ' + os.path.basename(sys.argv[0]) + '\n')
+        f.write('Evaluation started at       ' + str(rep_start) + '\n')
+        f.write('Evaluation ended at         ' + str(rep_end) + '\n')
+        f.write('Runtime:                    ' + str(time1 - time0) + '\n\n')
+        f.write('Agent Config File:          ' + str(args.agent_config_path) + '\n')
+        f.write('Map used:                   ' + str(mapin) + '\n\n')
+        f.write('Number of agents:           ' + str(number_of_agents) + '\n')
+        f.write('Agent Distribution:\n')
+        for key, value in agent_config['agent_distribution'].items():
+            f.write(f'  {key}: {round(value * 100, 2)}%\n')  # value is a fraction
+        f.write('Number of days:             ' + str(number_of_days) + '\n\n')
+        f.write('Routing written to          ' + str(out_path + routesout) + '\n')
+        f.write('Vehicle map written to      ' + str(out_path + vehmapout) + '\n\n')
+        f.write('-------------------------- END of report ---------------------------\n\n')
+    print("Report written to '" + rep_path + rep_name + "'")
 
 
-# Gets command line arguments using the argparse module
+# Gets command line arguments
 def get_options():
     parser = argparse.ArgumentParser(description='Parameters')
-    parser.add_argument('--inpath', dest='in_path', type=str, default='../rsc/traffic/', help='Relative path to resource file directory')
-    parser.add_argument('--outpath', dest='out_path', type=str, default='../rsc/traffic/', help='Relative path to output directory')
-    parser.add_argument('--agents', dest='number_of_agents', type=int, default=20, help='Number of agents')
+    parser.add_argument('--inpath', dest='in_path', type=str, default='../rsc/traffic/',
+                        help='Relative path to resource file directory')
+    parser.add_argument('--outpath', dest='out_path', type=str, default='../rsc/traffic/',
+                        help='Relative path to output directory')
+    parser.add_argument('--agents', dest='number_of_agents', type=int, default=20,
+                        help='Number of agents')
 
-    # --- ENTFERNTE ARGUMENTE ---
-    # Die Prozent-Argumente werden entfernt, da sie jetzt aus der YAML-Datei kommen
-    # parser.add_argument('--parttime', dest='parttime_percentage', type=int, default=10)
-    # parser.add_argument('--nighttime', dest='nighttime_percentage', type=int, default=10)
-    # parser.add_argument('--homestay', dest='homestay_percentage', type=int, default=15)
-    # parser.add_argument('--freelance', dest='freelance_percentage', type=int, default=15)
+    # Pfad zur YAML-Konfiguration der Agenten
+    parser.add_argument('--agentconfig', dest='agent_config_path', type=str,
+                        default='../rsc/config/agent_profiles.yaml',
+                        help='Path to agent profiles config YAML file')
 
-    # --- NEUES ARGUMENT ---
-    # Fügt ein Argument hinzu, um den Pfad zur neuen YAML-Konfigurationsdatei anzugeben
-    parser.add_argument('--agentconfig', dest='agent_config_path', type=str, default='../rsc/config/agent_profiles.yaml', help='Path to agent profiles config YAML file')
-
-    parser.add_argument('--days', dest='number_of_days', type=int, default=30, help='Number of days')
+    parser.add_argument('--days', dest='number_of_days', type=int, default=30,
+                        help='Number of days')
     parser.add_argument('--mapin', dest='map_input_name', type=str, default='map.xml')
     parser.add_argument('--routesout', dest='routes_output_name', type=str, default='routes.xml')
     parser.add_argument('--vehmapout', dest='vehicle_map_output_name', type=str, default='vehicle_map.csv')
     parser.add_argument('--homedistrict', dest='home_district', type=str, default='')
     parser.add_argument('--workdistrict', dest='work_district', type=str, default='')
-    parser.add_argument('--reportpath', dest='report_path', type=str, default='../rsc/reports/', help='Report output directory')
-    parser.add_argument('--reportname', dest='report_name', type=str, help='Set report name', default='report.txt')
+    parser.add_argument('--reportpath', dest='report_path', type=str, default='../rsc/reports/',
+                        help='Report output directory')
+    parser.add_argument('--reportname', dest='report_name', type=str, default='report.txt',
+                        help='Set report name')
     parser.add_argument('--no-report', dest='report', action='store_false', help='Do not write report')
     parser.set_defaults(report=True)
     return parser.parse_args()
 
 
 def string_to_polygon_array(polygon_string):
+    if not polygon_string:
+        return []
     dist_parse = polygon_string.split(' ')
     result = []
     for pt in dist_parse:
         coords = pt.split(',')
+        if len(coords) != 2:
+            continue
         result.append([int(coords[0]), int(coords[1])])
     return result
 
 
 if __name__ == "__main__":
-    # Copy args.arguments to ‘regular’ arguments
+    # CLI args
     args = get_options()
     in_path = args.in_path
     out_path = args.out_path
@@ -240,10 +254,9 @@ if __name__ == "__main__":
     rep_name = args.report_name
     number_of_agents = args.number_of_agents
 
-    # --- NEUE LOGIK ZUM LADEN DER KONFIGURATION ---
-    # Lade die Agenten-Konfigurationsdatei
+    # Agenten-Konfig laden
     try:
-        with open(args.agent_config_path, 'r') as f:
+        with open(args.agent_config_path, 'r', encoding='utf-8') as f:
             agent_config = yaml.safe_load(f)
     except FileNotFoundError:
         print(f"FATAL ERROR: Agent config file not found at: {args.agent_config_path}")
@@ -252,23 +265,18 @@ if __name__ == "__main__":
         print(f"FATAL ERROR: Could not parse YAML file: {e}")
         sys.exit(1)
 
-    # Berechne die Prozent-Summe der spezialisierten Agenten
-    specialized_percentage_sum = 0
-    for agent_type, percentage in agent_config['agent_distribution'].items():
-        agent_config['agent_distribution'][agent_type] = percentage / 100.0
-        specialized_percentage_sum += percentage / 100.0
+    # Prozentwerte in Brüche umrechnen und Summe prüfen
+    specialized_percentage_sum = 0.0
+    for agent_type, percentage in list(agent_config['agent_distribution'].items()):
+        frac = float(percentage) / 100.0
+        agent_config['agent_distribution'][agent_type] = frac
+        specialized_percentage_sum += frac
 
-    if specialized_percentage_sum > 1.0:
+    if specialized_percentage_sum > 1.0 + 1e-9:
         raise ValueError("Invalid percentages in YAML. Total of specialized agents exceeds 100%.")
 
-    # Füge den 'worker' als den verbleibenden Rest hinzu (z.B. 1.0 - 0.45 = 0.55)
-    # Das entspricht deinen 55%
-    agent_config['agent_distribution']['worker'] = 1.0 - specialized_percentage_sum
-
-    # --- ENTFERNTE LOGIK ---
-    # Die alten Prozent-Berechnungen werden entfernt
-    # parttime_percentage = args.parttime_percentage / 100.0
-    # ...
+    # Rest = Worker
+    agent_config['agent_distribution']['worker'] = max(0.0, 1.0 - specialized_percentage_sum)
 
     number_of_days = args.number_of_days
     mapin = args.map_input_name
--- location-privacy-etsi/agent/model/Freelance.py
+++ location-privacy-etsi/agent/model/Freelance.py
@@ -1,8 +1,4 @@
-from datetime import timedelta
-from datetime import time
-from datetime import datetime
-from datetime import date
-
+from datetime import timedelta, time, datetime, date
 import random
 import numpy as np
 
@@ -11,100 +7,107 @@ from model.AgentType import AgentType
 
 
 class Freelance(Agent):
-    # Constructor
-    # id: A vehicle ID
-    # home: A distinct home location
-    # leisure_locations: A list of locations for leisure (e.g., coffee, gym, park)
-    # config: Das Konfigurations-Profil aus der YAML-Datei
+    """
+    Freelance agent: works independently, has flexible schedules, and performs
+    optional leisure activities during the day.
+    """
+
     def __init__(self, vehicle_id, home, leisure_locations, config):
         super().__init__(vehicle_id, home)
         self.type = AgentType.FREELANCE
-        self.leisure_locations = leisure_locations
-        self.config = config # Speichert die Konfiguration
+        self.config = config
         self.leisure_times = []
 
-        # Generate some potential "activity times" for the whole simulation
-        # These will be randomly chosen from.
-        # --- NEU: Verwende Konfigurationswerte statt fester Zahlen ---
-        prev_time = config['activity']['time_min']  # z.B. 9 AM
-        max_time = config['activity']['time_max']   # z.B. 8 PM
-
-        if not leisure_locations:
+        # ✅ Fix: handle numpy arrays, None, or empty lists properly
+        if leisure_locations is None or len(leisure_locations) == 0:
+            self.leisure_locations = []
             return
+        else:
+            # convert np.ndarray to normal list for consistency
+            self.leisure_locations = list(leisure_locations)
 
-        for i in range(len(leisure_locations)):
-            leisure_remaining = (len(leisure_locations) - i)
-            # Ensure at least 30 min between activities
+        # Generate potential "activity times" based on config
+        prev_time = config['activity']['time_min']   # e.g. 9.0 (9 AM)
+        max_time = config['activity']['time_max']    # e.g. 20.0 (8 PM)
+
+        for i in range(len(self.leisure_locations)):
+            leisure_remaining = len(self.leisure_locations) - i
+            # ensure at least 30 min buffer per remaining activity
             remaining_time = max_time - prev_time - 0.5 * leisure_remaining
 
-            # Handle potential negative remaining_time if many locations are passed
+            # Prevent negative time intervals
             if remaining_time <= 0:
                 prev_time = random.uniform(prev_time, max_time)
             else:
                 prev_time = random.uniform(prev_time, prev_time + remaining_time / leisure_remaining)
 
-            prev_time = min(prev_time + 0.5, max_time) # ensure it doesn't go over max_time
+            # ensure time stays within max_time
+            prev_time = min(prev_time + 0.5, max_time)
             if prev_time >= max_time:
                 break
 
             self.leisure_times.append(self.time_from_float(prev_time))
 
     # Generate a day's worth of actions.
-    # This agent has no routine and behavior is the same on weekdays/weekends.
+    # This agent has no strict routine—behavior is the same on weekdays/weekends.
     def generate_day(self):
         actions = []
 
-        # --- KEY BEHAVIOR: 40% chance of staying home all day ---
-        # --- NEU: Verwende Konfigurationswert statt fester Zahl ---
-        if random.random() < self.config['stay_home_prob'] or not self.leisure_locations or not self.leisure_times:
+        # --- 40% chance (or config value) of staying home all day ---
+        if (
+                random.random() < self.config['stay_home_prob']
+                or not self.leisure_locations
+                or not self.leisure_times
+        ):
             self.end_day()
             return []
 
-        # Decide on 1 or 2 activities for the day
-        # --- NEU: Verwende Konfigurationswerte statt fester Zahlen ---
+        # Decide number of activities (bounded by config)
         num_activities = random.randint(
             self.config['activity']['num_activities_min'],
-            min(self.config['activity']['num_activities_max'], len(self.leisure_locations), len(self.leisure_times))
+            min(
+                self.config['activity']['num_activities_max'],
+                len(self.leisure_locations),
+                len(self.leisure_times)
+            )
         )
 
-        # Get a random subset of activities and their times
+        # Shuffle and select subset of activities
         zipped_list = list(zip(self.leisure_times, self.leisure_locations))
         random.shuffle(zipped_list)
         todays_activities = sorted(zipped_list[:num_activities])
 
-        # Use the same trip-chaining logic as other agents
-        # --- NEU: Verwende Konfigurationswert statt fester Zahl ---
-        chain_trips = random.random() < self.config['activity']['chain_trips_prob'] and num_activities > 1
+        # Determine if trips are chained
+        chain_trips = (
+                random.random() < self.config['activity']['chain_trips_prob']
+                and num_activities > 1
+        )
 
         if not chain_trips:
-            # --- Path 1: Separate Trips ---
+            # Separate trips (home → activity → home)
             for activity_time, activity_location in todays_activities:
                 self.set_time_t(activity_time)
-                # --- NEU: Verwende Konfigurationswerte statt fester Zahlen ---
                 stay_duration = self.timedelta_from_float(random.uniform(
                     self.config['activity']['stay_duration_min'],
                     self.config['activity']['stay_duration_max']
                 ))
-
                 a1 = self.advance_step(activity_location, stay_duration)
                 a2 = self.advance_step(self.home, timedelta(0))
                 actions.extend([a1, a2])
 
         else:
-            # --- Path 2: Chained Trips ---
+            # Chained trips (home → multiple activities → home)
             self.set_time_t(todays_activities[0][0])
 
-            for activity_time, activity_location in todays_activities:
-                # --- NEU: Verwende Konfigurationswerte statt fester Zahlen ---
+            for _, activity_location in todays_activities:
                 stay_duration = self.timedelta_from_float(random.uniform(
                     self.config['activity']['stay_duration_min'],
                     self.config['activity']['stay_duration_max']
                 ))
-
                 a_activity = self.advance_step(activity_location, stay_duration)
                 actions.append(a_activity)
 
-            # After the last activity, go home
+            # Return home at end
             a_home = self.advance_step(self.home, timedelta(0))
             actions.append(a_home)
 
